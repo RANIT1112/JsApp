f867418c52af5bba6b0b86581d88903e3397c198
RANIT1112 <ghoshranit7491@gmail.com>
2024-07-21 10:21:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-import java.util.*;
-class kamal{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter any integer : ");
-    int n = sc.nextInt();
-    System.out.println("Hello Suman Fuckers! "+n);
-    
-}
-}
